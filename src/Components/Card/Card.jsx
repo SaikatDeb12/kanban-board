12e363453261628080fceca3cc7c4746fb92c7af
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-27 14:53:32 +0530

    UI update

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Chip from "../Chip/Chip";
 import "./Card.scss";
 import { SlOptions } from "react-icons/sl";
+import { GoClock } from "react-icons/go";
+import { GoChecklist } from "react-icons/go";
 
 const Card = () => {
   return (
@@ -10,6 +12,17 @@ const Card = () => {
         <div className="card-label">
           <Chip text="Frontend" color="green" />
           <SlOptions className="options" />
+        </div>
+        <div className="card-title">Lorem ipsum dolor sit amet.</div>
+        <div className="card-footer">
+          <p>
+            <GoClock />
+            2nd Sept
+          </p>
+          <p>
+            <GoChecklist />
+            2/3
+          </p>
         </div>
       </div>
     </div>
